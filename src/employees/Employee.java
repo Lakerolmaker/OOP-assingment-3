55ec6899805e0c914977bc6eacce63b704033afa
Lakerolmaker <jacob.n.olsson@hotmail.com>
2017-10-02 18:42:18 +0200

    update

---
+++ src/employees/Employee.java
@@ -0,0 +1,49 @@
+package employees;
+//package employees;
+
+public class Employee {
+	
+	private String id;
+	private String employeeName;
+	private double grossSalary;
+	
+	public Employee(String id, String employeeName, double grossSalary) {
+		this.id = id;
+		this.employeeName = employeeName;
+		this.grossSalary = grossSalary;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public double getGrossSalary() {
+		return grossSalary;
+	}
+
+	public void setGrossSalary(double grossSalary) {
+		this.grossSalary = grossSalary;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public double getNetSalary() {
+		
+		double netSalary = grossSalary - (grossSalary * 0.1);
+		
+		return netSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee id: " + id + ", employee name: " + employeeName + ", gross salary: " + grossSalary + "]";
+	}
+	
+
+}
